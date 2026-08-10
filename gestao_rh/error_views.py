f50df338ea191c8cb3a998ad9581bf27b6abdef1
Renato <48660226+renatorl224370github@users.noreply.github.com>
2026-08-09 22:13:42 -0300

    Melhora logs de erros internos

--- gestao_rh/error_views.py
+++ gestao_rh/error_views.py
@@ -6,16 +6,16 @@ from django.shortcuts import render
 logger = logging.getLogger("django.request")
 
 
-def bad_request(request, exception):
-    return render(request, "400.html", status=400)
+def bad_request(request, exception=None):
+    return render(request, "errors/400.html", status=400)
 
 
-def permission_denied(request, exception):
-    return render(request, "403.html", status=403)
+def permission_denied(request, exception=None):
+    return render(request, "errors/403.html", status=403)
 
 
-def page_not_found(request, exception):
-    return render(request, "404.html", status=404)
+def page_not_found(request, exception=None):
+    return render(request, "errors/404.html", status=404)
 
 
 def server_error(request):
@@ -24,4 +24,4 @@ def server_error(request):
         request.path,
     )
 
-    return render(request, "500.html", status=500)
+    return render(request, "errors/500.html", status=500)
